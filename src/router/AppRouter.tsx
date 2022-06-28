444af805fecb5deb4b66f57f3991b06f01bf46a3
victor <>
2022-06-28 13:59:32 +0300

    [+] add Routing

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,13 +3,27 @@ import { LoginPage } from "../pages/LoginPage";
 import { CircularProgress } from "@mui/material";
 import { useUser } from "../hooks/useUser";
 import { ChatPage } from "../pages/ChatPage";
+import { FavoritePage } from "../pages/FavoritePage";
+import { DeletedPage } from "../pages/DeletedPage";
+import { MembersPage } from "../pages/MembersPage";
+import { useUrl } from "../hooks/useUrl";
+import { NotFoundPage } from "../pages/NotFoundPage";
+
+const Router: { [url: string]: () => JSX.Element } = {
+  "/message": ChatPage,
+  "/favorite": FavoritePage,
+  "/deleted": DeletedPage,
+  "/members": MembersPage,
+};
 
 export const AppRouter = () => {
+  const [page] = useUrl();
   const [user, loading] = useUser();
 
   if (loading) return <CircularProgress />;
   if (!user) return <LoginPage />;
-  if (!!user) return <ChatPage />;
 
-  return <LoginPage />;
+  const Page = Router[page] || NotFoundPage;
+
+  return <Page />;
 };
